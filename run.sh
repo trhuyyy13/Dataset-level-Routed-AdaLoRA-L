c4e9e0ea18c473a69f0d9d638f1fa2d8f6ccfae5
trhuyyy13 <huytran.soict.hust@gmail.com>
2026-04-05 16:56:42 +0700

    add

--- run.sh
+++ run.sh
@@ -1,5 +1,4 @@
 python edit_main.py \
   --editing_method=ROUTED_ADALORA \
   --model=deepseek-1.3b \
-  --suggest_layers=knowledge-locator/layer_importance/deepseek-1.3b/routed_layer_config.json \
-  --ds_size=25
+  --suggest_layers=knowledge-locator/layer_importance/deepseek-1.3b/routed_layer_config.json
