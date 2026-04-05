160aa661ced0e5f2378f03a2f6471c8aef8ae1ea
trhuyyy13 <huytran.soict.hust@gmail.com>
2026-04-05 16:03:03 +0700

    fix

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 python edit_main.py \
-  --editing_method=ADALORA \
+  --editing_method=ROUTED_ADALORA \
   --model=deepseek-1.3b \
-  --suggest_layers=knowledge-locator/layer_importance/deepseek-1.3b/suggest_edit_layers.json\
+  --suggest_layers=knowledge-locator/layer_importance/deepseek-1.3b/routed_layer_config.json
